b69847a6ac53b81edb39a329cebf2218624503eb
Yersultan <ersultansansyzbaj012@gmail.com>
2023-09-24 16:31:04 +0600

    add component cardslist, card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import { Hero } from "@/components";
-import Image from "next/image";
+import { Hero, CardsList } from "@/components";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <div className="container mx-auto sm:px-10 px-5 mb-8">
+        <CardsList />
+      </div>
+    </>
+  );
 }
